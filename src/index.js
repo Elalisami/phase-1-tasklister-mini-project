2f544aa1a7fa030056c81596c5e9a44b1bd869ab
Elalisami <ali.sami@ukh.edu.krd>
2022-10-17 20:06:23 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ const form= document.querySelector("form")
+ const addTask= document.querySelector("#tasks")
+ 
+ form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+ const newTask=document.createElement("li")
+ newTask.innerText=e.target[0].value;
+ addTask.appendChild(newTask);
+ })
 });
+
+
