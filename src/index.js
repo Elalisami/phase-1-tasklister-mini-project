7575b3e8a640036437a9a548aa3f0ef9aadb9a6e
Elalisami <ali.sami@ukh.edu.krd>
2022-10-17 20:36:50 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(e);
  const newTask=document.createElement("li")
  newTask.innerText=e.target[0].value;
+ const removeBtn= document.createElement("button");
+ removeBtn.innerHTML="remove"
+ removeBtn.addEventListener("click", deleteTask) 
+ newTask.appendChild(removeBtn);
  addTask.appendChild(newTask);
  })
 });
 
+function deleteTask(e){
+  console.log("Hello")
+  e.preventDefault();
+  e.currentTarget.parentElement.remove();
+}
 
